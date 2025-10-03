08d50d2ed3f723c141a1221e878a2bccc90f2ce7
Pedro Barros <pedrobars7a@gmail.com>
2025-10-03 16:01:11 -0300

    feature: removendo servico e controller nao usados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { Ordem } from './ordens/entities/ordem.entity';
 import { OrdensModule } from './ordens/ordens.module';
@@ -31,7 +29,7 @@ import { UsersModule } from './users/users.module';
     UsersModule,
     OrdensModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
